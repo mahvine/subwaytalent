12bb0d8d2ce2b6ef8f6c70e6e03898879952db56
mahvine <joshvindomingo@gmail.com>
2016-12-08 16:33:49 +0800

    initial commit

---
+++ src/test/java/edu/ust/subway/talent/SubwaytalentApplicationTests.java
@@ -0,0 +1,16 @@
+package edu.ust.subway.talent;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SubwaytalentApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
